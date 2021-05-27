0eedfcd1f4dae88b68bbe33fe416a584216c6631
Logan-Arias-LPS <71857092+Logan-Arias-LPS@users.noreply.github.com>
2021-05-27 11:16:25 -0400

    Update release.bat
    fix relase.bat

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @echo off
-git archive --format zip --output dists\LPS-GeneralDemographics-Custom.zip --worktree-attributes --verbose -9 HEAD
+git archive --format zip --output dists\LPS-EmailConfiguration-Custom.zip --worktree-attributes --verbose -9 HEAD
 pause
